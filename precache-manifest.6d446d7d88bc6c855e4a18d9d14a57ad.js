b5a923a02f0a455899a3b25439e40f36b559b5dd
Astellarius <zak.lake0@gmail.com>
2019-08-28 02:44:19 -0700

    Updates

---
+++ precache-manifest.6d446d7d88bc6c855e4a18d9d14a57ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78ab3e03538c26723c40b67758261d2",
+    "url": "/react-astellarius/index.html"
+  },
+  {
+    "revision": "5041c9667e1655d847de",
+    "url": "/react-astellarius/static/css/main.0a0d8abf.chunk.css"
+  },
+  {
+    "revision": "2c9539e0474598defd42",
+    "url": "/react-astellarius/static/js/2.45975a7f.chunk.js"
+  },
+  {
+    "revision": "5041c9667e1655d847de",
+    "url": "/react-astellarius/static/js/main.4cbcb176.chunk.js"
+  },
+  {
+    "revision": "69362df4cdbc9d3cf553",
+    "url": "/react-astellarius/static/js/runtime~main.f02db357.js"
+  },
+  {
+    "revision": "acb0f4b451046c648627ecb627424e1e",
+    "url": "/react-astellarius/static/media/bebop.acb0f4b4.gif"
+  },
+  {
+    "revision": "2beb025c91d863039733d4b4e80f6114",
+    "url": "/react-astellarius/static/media/logosm.2beb025c.png"
+  },
+  {
+    "revision": "f1bd5006a8f4d8188f5f062cd33c328a",
+    "url": "/react-astellarius/static/media/sfusm.f1bd5006.jpg"
+  }
+]);
